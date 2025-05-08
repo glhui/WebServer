135c82ba54d59d4f38007852e032fe762bd3e881
wsl-genglinhui <2661305317@qq.com>
2025-05-08 18:04:25 +0800

    一些增加

--- server/include/Channel.h
+++ server/include/Channel.h
@@ -11,28 +11,8 @@ class EventLoop;
 class HttpData;
 
 class Channel {
-public:
-    Channel(EventLoop* loop);
-    Channel(EventLoop* loop, int fd);
-    ~Channel();
-    int getFd();
-    void setFd(int fd);
-
-    std::shared_ptr<HttpData> getHolder() {
-        std::shared_ptr<HttpData> ret(holder_.lock());
-        return ret;
-    }
-
-    __uint32_t &getEvents() { return events_; }
-
-    bool EqualAndUpdateLastEvents() {
-        bool ret = (lastEvents_ == events_);
-        lastEvents_ = events_;
-        return ret;
-      }
-    
-      __uint32_t getLastEvents() { return lastEvents_; }
 private:
+    // private members
     typedef std::function<void()> CallBack;
     EventLoop *loop_;
     int fd_;
@@ -41,6 +21,85 @@ private:
     __uint32_t lastEvents_;
 
     std::weak_ptr<HttpData> holder_;
+
+    // private functions
+    int parse_URI();
+    int parse_Headers();
+    int analysisRequest();
+
+    CallBack readHandler_;
+    CallBack writeHandler_;
+    CallBack errorHandler_;
+    CallBack connHandler_;
+public:
+    Channel(EventLoop* loop);
+    Channel(EventLoop* loop, int fd);
+    ~Channel();
+    int getFd();
+    void setFd(int fd);
+
+    void setHolder(std::shared_ptr<HttpData> holder) { 
+        holder_ = holder; 
+    }
+
+    std::shared_ptr<HttpData> getHolder() {
+        std::shared_ptr<HttpData> ret(holder_.lock());
+        return ret;
+    }
+
+    void setReadHandler(CallBack &&readHandler) { 
+        readHandler_ = readHandler; 
+    }
+    void setWriteHandler(CallBack &&writeHandler) {
+        writeHandler_ = writeHandler;
+    }
+    void setErrorHandler(CallBack &&errorHandler) {
+        errorHandler_ = errorHandler;
+    }
+    void setConnHandler(CallBack &&connHandler) { 
+        connHandler_ = connHandler; 
+    }
+
+    void handleEvents() {
+        events_ = 0;
+        if ((revents_ & EPOLLHUP) && !(revents_ & EPOLLIN)) {
+            events_ = 0;
+            return;
+        }
+        if (revents_ & EPOLLERR) {
+            if (errorHandler_) {
+                errorHandler_();
+            }
+            events_ = 0;
+            return;
+        }
+        if (revents_ & (EPOLLIN | EPOLLPRI | EPOLLRDHUP)) {
+            handleRead();
+        }
+        if (revents_ & EPOLLOUT) {
+            handleWrite();
+        }
+        handleConn();
+    }
+    void handleRead();
+    void handleWrite();
+    void handleError(int fd, int err_num, std::string short_msg);
+    void handleConn();
+
+    void setRevents(__uint32_t ev) { revents_ = ev; }
+    void setEvents(__uint32_t ev) { events_ = ev; }
+
+    __uint32_t &getEvents() { return events_; }
+
+    bool EqualAndUpdateLastEvents() {
+        bool ret = (lastEvents_ == events_);
+        lastEvents_ = events_;
+        return ret;
+    }
+    
+    __uint32_t getLastEvents() { 
+        return lastEvents_; 
+    }
 };
 
 typedef std::shared_ptr<Channel> SP_Channel;
--- server/src/Epoll.cpp
+++ server/src/Epoll.cpp
@@ -46,3 +46,70 @@ void Epoll::epoll_add(SP_Channel request, int timeout) {
         fd2chan_[fd].reset();
     }
 }
+
+void Epoll::epoll_mod(SP_Channel request, int timeout) {
+    if (timeout > 0) {
+        add_timer(request, timeout);
+    }
+    int fd = request->getFd();
+    if (!request->EqualAndUpdateLastEvents()) {
+        struct epoll_event event;
+        event.data.fd = fd;
+        event.events = request->getEvents();
+        if (epoll_ctl(epollFd_, EPOLL_CTL_MOD, fd, &event) < 0) {
+            perror("epoll_mod error");
+            fd2chan_[fd].reset();
+        }
+    }
+}
+
+void Epoll::epoll_del(SP_Channel request) {
+    int fd = request->getFd();
+    struct epoll_event event;
+    event.data.fd = fd;
+    event.events = request->getEvents();
+    if (epoll_ctl(epollFd_, EPOLL_CTL_DEL, fd, &event) < 0) {
+        perror("epoll_del error");
+    }
+    fd2chan_[fd].reset();
+    fd2http_[fd].reset();
+    
+}
+
+std::vector<SP_Channel> Epoll::poll() {
+    while (true) {
+      int event_count = epoll_wait(epollFd_, &*events_.begin(), events_.size(), EPOLLWAIT_TIME);
+      if (event_count < 0) perror("epoll wait error");
+      std::vector<SP_Channel> req_data = getEventsRequest(event_count);
+      if (req_data.size() > 0) return req_data;
+    }
+}
+
+void Epoll::handleExpired() { 
+    // timerManager_.handleExpiredEvent(); 
+}
+
+std::vector<SP_Channel> Epoll::getEventsRequest(int events_num) {
+    std::vector<SP_Channel> req_data;
+    for (int i = 0; i < events_num; ++i) {
+        int fd = events_[i].data.fd;
+        SP_Channel cur_req = fd2chan_[fd];
+        if (cur_req) {
+            cur_req->setRevents(events_[i].events);
+            cur_req->setEvents(0);
+            req_data.push_back(cur_req);
+        } else {
+            LOG << "SP_Channel is null";
+        }
+    }
+    return req_data;
+}
+
+void Epoll::add_timer(SP_Channel request_data, int timeout) {
+    shared_ptr<HttpData> t = request_data->getHolder();
+    if (t) {
+        // timerManager_.addTimer(t, timeout);
+    } else {
+        LOG << "timer add fail";
+    }
+  }
